33e1974ef373dbf499c6a87ccd4ee2b44b294f65
VectorWing <pudim.antiaereo@gmail.com>
2014-12-17 16:48:50 -0200

    Update MainGame

--- src/com/vectorwing/games/minesweeper/logic/MainGame.java
+++ src/com/vectorwing/games/minesweeper/logic/MainGame.java
@@ -8,9 +8,11 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 import javax.swing.ImageIcon;
+import javax.swing.Timer;
 
 import com.vectorwing.games.minesweeper.enums.GameLevel;
 import com.vectorwing.games.minesweeper.enums.GameState;
@@ -27,17 +29,22 @@ public class MainGame {
 	
 	public static final int MINE = -1;
 	
+	// Widgets
 	private MainGUI							gui;
 	private GameLevel						level;
+	
+	// Arrays
+	private ArrayList<ArrayList<Integer>>	list_hints;
+	private ArrayList<ArrayList<Boolean>>	list_mines;	
+	
+	// Game Data
 	private GameState						state;
-	
-	private ArrayList<ArrayList<Integer>>	tile_content;
-	private ArrayList<Point>				mine_coords;
-	private ArrayList<ArrayList<Boolean>>	list_mines;
-	
 	private int								qt_tile_x;
 	private int								qt_tile_y;
 	private int								qt_mines;
+	private int								count_flags;
+	private Timer							count_time;
+	private int								count_triggered;
 	private boolean							custom;
 	
 	private BufferedImage[]	img_tile;
@@ -46,18 +53,23 @@ public class MainGame {
 	{
 		this.gui = gui;
 		this.setNormalGame(default_level);
+		
+		this.list_mines = new ArrayList<ArrayList<Boolean>>();
+		this.list_hints = new ArrayList<ArrayList<Integer>>();
 	}
 	
+	/** Sets one of the default game modes. Those have high-scores. **/
 	public void setNormalGame(GameLevel level)
 	{
 		this.custom = false;
 		this.level = level;
 		
-		this.qt_tile_x = level.qt_tiles_x;
-		this.qt_tile_y = level.qt_tiles_y;
-		this.qt_mines = level.qt_mines;
+		this.qt_tile_x = this.level.qt_tiles_x;
+		this.qt_tile_y = this.level.qt_tiles_y;
+		this.qt_mines = this.level.qt_mines;
 	}
 	
+	/** Sets a game with custom-tailored settings. Those do not hold high-scores. **/
 	public void setCustomGame(int qt_tile_x, int qt_tile_y, int qt_mines)
 	{
 		this.custom = true;
@@ -69,15 +81,24 @@ public class MainGame {
 		this.qt_mines = (qt_mines > max_mines) ? max_mines : qt_mines;
 	}
 	
+	/** Initiates a new game. Resets all counters, then distributes tiles. **/
 	public void begin()
 	{
 		this.state = GameState.PRE_GAME;
+		
+		this.list_mines.clear();
+		this.list_hints.clear();
+		this.count_flags = 0;
+		this.count_flags = 0;
 		
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(0, 0, 0, 0);
 		
 		for (int iter_y = 0; iter_y < this.qt_tile_y; iter_y++)
 		{
+			ArrayList<Boolean> minerow = new ArrayList<Boolean>(Collections.nCopies(qt_tile_x, false));
+			this.list_mines.add(minerow);
+			
 			gbc.gridy = iter_y;
 			ArrayList<Tile> tilerow = new ArrayList<Tile>();
 			for (int iter_x = 0; iter_x < this.qt_tile_x; iter_x++)
@@ -92,24 +113,50 @@ public class MainGame {
 			}
 			gui.getTileGrid().getTileArray().add(tilerow);
 		}
-		
-		// TODO Distribute mines across TileGrid and increment surrounding hints
 	}
 	
+	/** Ends the current game under the given state. If not VICTORY or DEFEAT, nothing happens. **/
+	public void finish(GameState final_state)
+	{
+		if (this.state != GameState.PLAYING)
+			return;
+		
+		switch (final_state)
+		{
+		case VICTORY:
+			// TODO: Congratulate player and attempt high-score update.
+			if (!this.custom)
+			{
+				System.out.println("NO HISCORS FOR YA");
+			} else {
+				System.out.println("YU ARE TOP WINNER");
+			}
+			break;
+		case DEFEAT:
+			// TODO: Display all mines and declare game over.
+			break;
+		default:
+			return;
+		}
+	}
+	
+	/** Called upon the first click. Distributes mines, as well as the hints around them. **/
 	private void deployMines(Point first_click)
 	{
+		if (this.state != GameState.PRE_GAME)
+			return;
+		
 		for (int i = 0; i < qt_mines; i++)
 		{
 			int row = randomInt(0, qt_tile_y);
 			int col = randomInt(0, qt_tile_x);
-			if (this.tile_content.get(row).get(col) != MINE)
+			if (!this.list_mines.get(row).get(col))
 			{
 				if (row == first_click.getY() && col == first_click.getX()) {
 					i -= 1;
 					continue;
 				} else {
-					this.tile_content.get(row).set(col, MINE);
-					this.mine_coords.add(new Point(col, row));
+					this.list_mines.get(row).set(col, true);
 				}
 			} else {
 				i -= 1;
@@ -117,33 +164,35 @@ public class MainGame {
 			}
 			
 			if (row > 0) {
-				this.tile_content.get(row-1).set(col, this.tile_content.get(row-1).get(col) + 1);
+				this.list_hints.get(row-1).set(col, this.list_hints.get(row-1).get(col) + 1);
 				if (col > 0) 
-					this.tile_content.get(row-1).set(col-1, this.tile_content.get(row-1).get(col-1) + 1);
+					this.list_hints.get(row-1).set(col-1, this.list_hints.get(row-1).get(col-1) + 1);
 				if (col < qt_tile_x-1)
-					this.tile_content.get(row-1).set(col+1, this.tile_content.get(row-1).get(col+1) + 1);
+					this.list_hints.get(row-1).set(col+1, this.list_hints.get(row-1).get(col+1) + 1);
 			}
 			if (row < qt_tile_y-1) {
-				this.tile_content.get(row+1).set(col, this.tile_content.get(row-1).get(col) + 1);
+				this.list_hints.get(row+1).set(col, this.list_hints.get(row-1).get(col) + 1);
 				if (col > 0)
-					this.tile_content.get(row+1).set(col-1, this.tile_content.get(row-1).get(col-1) + 1);
+					this.list_hints.get(row+1).set(col-1, this.list_hints.get(row-1).get(col-1) + 1);
 				if (col < qt_tile_x-1)
-					this.tile_content.get(row+1).set(col+1, this.tile_content.get(row-1).get(col+1) + 1);
+					this.list_hints.get(row+1).set(col+1, this.list_hints.get(row-1).get(col+1) + 1);
 			}
 			if (col > 0) {
-				this.tile_content.get(row).set(col-1, this.tile_content.get(row-1).get(col-1) + 1);
+				this.list_hints.get(row).set(col-1, this.list_hints.get(row-1).get(col-1) + 1);
 			}
 			if (col < qt_tile_x-1) {
-				this.tile_content.get(row).set(col+1, this.tile_content.get(row-1).get(col+1) + 1 );
+				this.list_hints.get(row).set(col+1, this.list_hints.get(row-1).get(col+1) + 1 );
 			}
 		}
 	}
 	
+	/** Generates a random integer within given range. **/
 	public static int randomInt(int min, int max) {
 		int randomNum = new Random().nextInt((max - min)) + min;
 	    return randomNum;
 	}
 	
+	/** A MouseAdapter designed to trigger behaviors when a tile is clicked. **/
 	class TileMouseAdapter extends MouseAdapter
 	{
 		private Tile tile;
@@ -165,7 +214,7 @@ public class MainGame {
 	       	if (state != GameState.DEFEAT && state != GameState.VICTORY)
 	       	{
 	       		if (state == GameState.PRE_GAME) {
-	       			//initMines(tile.getPosition());
+	       			deployMines(tile.getPosition());
 	       			state = GameState.PLAYING;
 	       		}
 	       		if (e.getButton() == MouseEvent.BUTTON1) {
